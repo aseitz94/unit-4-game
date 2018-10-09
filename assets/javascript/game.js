c259ede09329ddc6886690f9aa08112f561463ae
AshSeitz <ash.seitz94@gmail.com>
2018-10-09 13:47:02 -0400

    javascript layout planned

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//generate random number between 19-120
+
+//generate random number for each crystal between 1-12
+
+    //onclick crystals to add number to player's points
+
+    //hide values until crystals are clicked
+
+//win  IF player's score matches random chosen number
+
+//lose IF player goes over chosen number 
+
+//score is kept of the number of wins and loses
+
+//restart game when player wins or loses
+
+    //crystals have new hidden values
+    
+
+    //new random number generated
+
+    //score counter sets back to 0
